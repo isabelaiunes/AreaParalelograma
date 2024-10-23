7945a1c230f0fcdae76fa27ba9c6b1117cf2ccf4
Isabela Iunes <iunesisabela.cs@gmail.com>
2024-10-23 10:34:26 -0300

    master

--- areaparalelograma.py
+++ areaparalelograma.py
@@ -1,5 +1,9 @@
+# Saudações #
 print("Bem-vindo ao Calculador de Área de Paralelograma!")
+# Obtenha os dados do usuário e armazene-os #
 base = float(input("Insira o comprimento da base: "))
 altura = float(input("Insira a altura da base: "))
+# A área de um paralelograma é calculada multiplicando o valor da base pela altura #
 area = base * altura
+# Imprima o resultado #
 print("A área do paralelograma é: ", area)
